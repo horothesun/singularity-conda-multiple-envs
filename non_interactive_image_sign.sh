d677a7360507f9ade967766602b2d9f6f01154c6
horothesun <horothesun@gmail.com>
2021-03-21 14:12:13 +0000

    Image build GitHub Actions: Singularity Cloud login

--- non_interactive_image_sign.sh
+++ non_interactive_image_sign.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/expect
 
-echo "IMAGE_FILENAME: $IMAGE_FILENAME"
-echo "SINGULARITY_PGP_PASSPHRASE: $SINGULARITY_PGP_PASSPHRASE"
-
 set timeout 60
 
-spawn singularity sign $IMAGE_FILENAME
+spawn singularity sign $env(IMAGE_FILENAME)
 
 expect "Enter key passphrase : "
-send -- "$SINGULARITY_PGP_PASSPHRASE\r"
+send $env(SINGULARITY_PGP_PASSPHRASE)\r
 
 expect eof
