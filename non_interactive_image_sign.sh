fbdd70f6b0cc02d5086bfcf10d17be479faa2b7f
horothesun <horothesun@gmail.com>
2021-03-21 01:58:02 +0000

    Image build GitHub Actions: Singularity Cloud login

--- non_interactive_image_sign.sh
+++ non_interactive_image_sign.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/expect
 
+echo "IMAGE_FILENAME: $IMAGE_FILENAME"
+echo "SINGULARITY_PGP_PASSPHRASE: $SINGULARITY_PGP_PASSPHRASE"
+
 set timeout 60
 
 spawn singularity sign $IMAGE_FILENAME
