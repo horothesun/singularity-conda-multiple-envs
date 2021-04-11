e33bb010fcb47bb94ab792dabd23c9795e687ee2
horothesun <horothesun@gmail.com>
2021-04-11 14:37:50 +0100

    create_conda_envs.sh update

--- create_conda_envs.sh
+++ create_conda_envs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Start create_conda_envs.sh..."
+
+conda -V
+
 conda env update --file /environment-01.yml
 conda env update --file /environment-02.yml
 conda env update --file /environment-03.yml
+
+echo "... end create_conda_envs.sh"
